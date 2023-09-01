8eef884353bf697c2e33a7f7ec435b029a2a305e
Rona <inusarona@gmail.com>
2023-08-31 20:46:33 -0700

    Initial commit

---
+++ codewars/js/main.js
@@ -0,0 +1,14 @@
+//Thurs 8/31 Codewars
+
+//Task
+//An isogram is a word that has no repeating letters, 
+//consecutive or non-consecutive. Implement a function 
+//that determines whether a string that contains only 
+//letters is an isogram. Assume the empty string is 
+//an isogram. Ignore letter case.
+
+//code
+
+function isIsogram(str){
+    return new Set(str.toLowerCase()).size === str.length
+    }
