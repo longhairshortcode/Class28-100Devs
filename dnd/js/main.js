1275d19f9b71a721a562dc51fa63c3c8d77582a9
Rona <inusarona@gmail.com>
2023-09-05 12:17:30 -0700

    this is the second time i tried the d&d code

--- dnd/js/main.js
+++ dnd/js/main.js
@@ -1,3 +1,60 @@
+//on 9/5
+
+//Example fetch using DnD5eAPI - place subclasses in ul
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch(){
+  const choice = document.querySelector('input').value
+  const url = `https://www.dnd5eapi.co/api/spells/${choice}`
+  
+  // Clear the ul/li when you click:
+  document.querySelector('ul').innerHTML = '';
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+       console.log(data.subclasses)
+
+       data.subclasses.forEach( obj => {console.log(obj.name)
+        const li = document.createElement('li')
+        li.textContent = obj.name
+        document.querySelector('ul').appendChild(li)
+    }) 
+
+    //    document.querySelector('ul').appendChild(li) = data.subclasses[0]
+    //    document.querySelector('ul').appendChild(li) = data.subclasses[0]
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
+
+//new edit 9/5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Before 9/5
+
 //Example fetch using DnD5eAPI - place subclasses in ul
 document.querySelector('button').addEventListener('click', getFetch)
 
